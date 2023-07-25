a706074f36ae413ba21f5fb0dd3d1d4e2dfc45fd
TheZero <115753420+TheZeroMvP@users.noreply.github.com>
2023-07-25 12:43:45 -0500

    Update Main.php

--- src/LobbyCore/Main.php
+++ src/LobbyCore/Main.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace LobbyCore;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\Server;
+
+class LobbyCore implements Listener {
+
+    private $pene;
+    private $lobbySpawn;
+
+    public function __construct(YourPlugin $pene) {
+        $this->plugin = $plugin;
+        $this->lobbySpawn = $plugin->getServer()->getDefaultLevel()->getSafeSpawn();
+        $this->plugin->getServer()->getPluginManager()->registerEvents($this, $this->plugin);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $player->teleport($this->lobbySpawn);
+
+    }
+}
